319e7c065929a1300e21e0af0f5091274767295d
Edner Paulo Severo Dias <163218158+EdnerPaulo@users.noreply.github.com>
2026-05-21 18:54:30 -0300

    Update app_view.py

--- app/views/app_view.py
+++ app/views/app_view.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import os
 
-API_URL = "https://crud-livros-3dg3.onrender.com"
+API_URL = "https://match-livros.onrender.com"
 
 st.set_page_config(page_title="Biblioteca Digital", layout="wide")
 
